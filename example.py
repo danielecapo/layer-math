ebc66b61c18567642229254c29b55f539b25c404
danielecapo <capo.daniele@gmail.com>
2012-10-14 17:11:25 +0200

    fixed error in line 9

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ font = fontforge.activeFont()
 sum, scale = layermath.operation_space (font, ['n', 'o'])
 
 layermath.add_glyphs (font, sum ('weight', 'width'), "boldext")
-layermath.add_glyphs (font, sum (scale ('weight' 0.4, 0.9),\
+layermath.add_glyphs (font, sum (scale ('weight', 0.4, 0.9),\
                                      scale ('width', 0.2), scale ('square', 0.1)), \
                           "bold1")
 
